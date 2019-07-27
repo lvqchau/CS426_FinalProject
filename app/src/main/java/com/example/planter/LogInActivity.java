3ef7c60a18804687607b2e065320a4c26d11f6a2
lvqchau <lvqchau@apcs.vn>
2019-07-27 19:10:47 +0700

    Change Login layout

--- app/src/main/java/com/example/planter/LogInActivity.java
+++ app/src/main/java/com/example/planter/LogInActivity.java
@@ -4,8 +4,10 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +17,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,17 +26,18 @@ import java.util.List;
 
 public class LogInActivity extends AppCompatActivity {
 
+    private TextInputLayout textInputUsername, textInputUsernameLI;
+    private TextInputLayout textInputPass, textInputPassLI;
     private TextInputLayout textInputEmail;
-    private TextInputLayout textInputUsername;
     private TextInputLayout textInputFirstname;
     private TextInputLayout textInputLastname;
-    private TextInputLayout textInputPass;
+
+    public LinearLayout loginLayout, signupLayout;
+    public Button btnSignUp, btnLogIn, btnCheck, btnInsert, btnGo;
+
     private TextView textView;
 
-    private TextInputLayout textInputUsernameLI;
-    private TextInputLayout textInputPassLI;
     DatabaseHelper db;
-    Button btnCheck, btnInsert, btnGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +45,10 @@ public class LogInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         db = new DatabaseHelper(this);
 
+        btnLogIn = findViewById(R.id.login);
+        btnSignUp = findViewById(R.id.signup);
+        signupLayout = findViewById(R.id.SU_layout);
+        loginLayout = findViewById(R.id.LI_layout);
 
         textInputEmail = findViewById(R.id.SU_email);
         textInputFirstname = findViewById(R.id.SU_first);
@@ -57,9 +65,11 @@ public class LogInActivity extends AppCompatActivity {
         btnGo = findViewById(R.id.btnApp);
 
         btnOnClick();
+        toggleLogIn();
 
     }
 
+    //Click button to Sign up or to Log in
     public void btnOnClick() {
         btnGo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,17 +82,36 @@ public class LogInActivity extends AppCompatActivity {
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validateSignUp(view);
+                if (validateSignUp(view)) {
+                    String username = textInputUsername.getEditText().getText().toString().trim();
+                    String password = textInputPass.getEditText().getText().toString().trim();
+                    String email = textInputEmail.getEditText().getText().toString().trim();
+                    String first = textInputFirstname.getEditText().getText().toString().trim();
+                    String last = textInputLastname.getEditText().getText().toString().trim();
+                    long val = db.addUser(username, password, email, first, last);
+                    if (val > 0) {
+                        Toast.makeText(LogInActivity.this, "successful sign up", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LogInActivity.this, "sign up fail", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Toast.makeText(LogInActivity.this, "log in fail", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         btnCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = textInputUsernameLI.getEditText().getText().toString().trim();
-                String password = textInputPassLI.getEditText().getText().toString().trim();
-                Boolean res = db.checkUser(username, password);
-                if (res) {
-                    Toast.makeText(LogInActivity.this, "successful log in", Toast.LENGTH_SHORT).show();
+                if (validateLogIn(view)) {
+                    String username = textInputUsernameLI.getEditText().getText().toString().trim();
+                    String password = textInputPassLI.getEditText().getText().toString().trim();
+                    Boolean res = db.checkUser(username, password);
+                    if (res) {
+                        Toast.makeText(LogInActivity.this, "successful log in", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LogInActivity.this, "log in fail", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
                     Toast.makeText(LogInActivity.this, "log in fail", Toast.LENGTH_SHORT).show();
                 }
@@ -90,69 +119,87 @@ public class LogInActivity extends AppCompatActivity {
         });
     }
 
+    //Validate: if the field is not inputted, will raise error
+    public boolean validateSignUp(View v) {
+//        if (!validateEmail() | !validatePassword() | !validateName() | !validateUsername() | !validateUsername()) {
+        if (!validatePassword(textInputPass) | !validateUsername(textInputUsername) | !validateEmail()) {
+            return false;
+        }
+        return true;
+
+    }
+    public boolean validateLogIn(View v) {
+        if (!validatePassword(textInputPassLI) | !validateUsername(textInputUsernameLI)) {
+            return false;
+        }
+        return true;
+    }
 
     private boolean validateEmail() {
         String emailInput = textInputEmail.getEditText().getText().toString().trim();
         if (emailInput.isEmpty()) {
-            textInputEmail.setError("(*) Field can not be empty!");
+            textInputEmail.setError("e");
         } else {
             textInputEmail.setError(null);
             return true;
         }
         return false;
     }
-    private boolean validateUsername() {
-        String usernameInput = textInputUsername.getEditText().getText().toString().trim();
+    private boolean validateUsername(TextInputLayout textInputLayout) {
+        String usernameInput = textInputLayout.getEditText().getText().toString().trim();
         if (usernameInput.isEmpty()) {
-            textInputUsername.setError("(*) Field can not be empty!");
-        } else if (usernameInput.length() > 15) {
-            textInputUsername.setError("(*) Username must be less than 16 characters");
+            textInputLayout.setError("e");
         } else {
-            textInputUsername.setError(null);
+            textInputLayout.setError(null);
             return true;
         }
         return false;
     }
-//    private boolean validateName() {
-//        String firstInput = textInputFullname.getEditText().getText().toString().trim();
-//        if (firstInput.isEmpty()) {
-//            textInputFullname.setError("(*) Field can not be empty!");
-//        } else {
-//            textInputFullname.setError(null);
-//            return true;
-//        }
-//        return false;
-//    }
-    private boolean validatePassword() {
-        String passInput = textInputPass.getEditText().getText().toString().trim();
+    private boolean validatePassword(TextInputLayout textInputLayout) {
+        String passInput = textInputLayout.getEditText().getText().toString().trim();
         if (passInput.isEmpty()) {
-            textInputPass.setError("(*) Field can not be empty!");
+            textInputLayout.setError("e");
         } else if (passInput.length() > 12) {
-            textInputPass.setError("(*) Password must be less than 13 characters");
+            textInputLayout.setError("e");
         } else {
-            textInputPass.setError(null);
+            textInputLayout.setError(null);
             return true;
         }
         return false;
     }
 
-
-    public void validateSignUp(View v) {
-//        if (!validateEmail() | !validatePassword() | !validateName() | !validateUsername() | !validateUsername()) {
-        if (!validatePassword() | !validateUsername() | !validateEmail()) {
-            return;
-        }
-        String username = textInputUsername.getEditText().getText().toString().trim();
-        String password = textInputPass.getEditText().getText().toString().trim();
-        String email = textInputEmail.getEditText().getText().toString().trim();
-        String first = textInputFirstname.getEditText().getText().toString().trim();
-        String last = textInputLastname.getEditText().getText().toString().trim();
-        long val = db.addUser(username, password, email, first, last);
-        if (val > 0) {
-            Toast.makeText(LogInActivity.this, "successful sign up", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(LogInActivity.this, "sign up fail", Toast.LENGTH_SHORT).show();
-        }
-
+    //change between Log in and Sign Up layout
+    public void toggleLogIn() {
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchLayoutOnLaunch(btnSignUp, btnLogIn, signupLayout, loginLayout);
+            }
+        });
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchLayoutOnLaunch(btnLogIn, btnSignUp, loginLayout, signupLayout);
+            }
+        });
     }
+    public void switchLayoutOnLaunch(Button btnCur, Button btnPrev, LinearLayout cur, LinearLayout prev) {
+        btnCur.setBackgroundResource(R.drawable.topcorner);
+        btnCur.setTextColor(Color.parseColor("#000000"));
+        if (btnCur == btnSignUp) {
+            btnCur.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_signup_selected, 0, 0, 0);
+            btnPrev.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_login, 0, 0, 0);
+        } else if (btnCur == btnLogIn) {
+            btnCur.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_login_selected, 0, 0, 0);
+            btnPrev.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_signup, 0, 0, 0);
+        }
+
+        btnPrev.setBackgroundColor(Color.parseColor("#ffffff"));
+        btnPrev.setTextColor(Color.parseColor("#8F8F8F"));
+
+
+        cur.setVisibility(View.VISIBLE);
+        prev.setVisibility(View.GONE);
+    }
+
 }
